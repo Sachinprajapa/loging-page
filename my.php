241eede41b930850f3c5c95f9b0a462bafdb7457
Sachinprajapa <166122249+Sachinprajapa@users.noreply.github.com>
2024-07-27 10:29:12 +0530

    Add files via upload

---
+++ my.php
@@ -0,0 +1,12 @@
+<?php
+    $name=$_POST['name'];
+    echo $name;
+    $password=$_POST['password'];
+    echo $password;
+    $email=$_POST['email'];
+    echo $email;
+    $city=$_POST['city'];
+    echo $city;
+
+  
+  ?>
